05273876e0230a21a2cfe7d414a405fae19527db
Alberto Cole <acpii2005@gmail.com>
2014-11-05 00:05:00 -0600

    Agregando codigo que envia coordenadas en background (6)

--- app/src/main/java/com/amatista/geoloctracker/TrackerActivity.java
+++ app/src/main/java/com/amatista/geoloctracker/TrackerActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.client.Firebase;
@@ -16,10 +17,13 @@ import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.location.LocationClient;
 
 import com.amatista.geoloctracker.CoordinateObject;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 
 public class TrackerActivity extends ActionBarActivity implements
         GooglePlayServicesClient.ConnectionCallbacks,
-        GooglePlayServicesClient.OnConnectionFailedListener  {
+        GooglePlayServicesClient.OnConnectionFailedListener,
+        LocationListener {
 
     private Firebase ref;
     private static final String FIREBASE_URL = "https://sabanacementerio.firebaseio.com";
@@ -37,10 +41,35 @@ public class TrackerActivity extends ActionBarActivity implements
 
     static final int REQUEST_CODE_RECOVER_PLAY_SERVICES = 1001;
 
+    // Milliseconds per second
+    private static final int MILLISECONDS_PER_SECOND = 1000;
+    // Update frequency in seconds
+    public static final int UPDATE_INTERVAL_IN_SECONDS = 5;
+    // Update frequency in milliseconds
+    private static final long UPDATE_INTERVAL =
+            MILLISECONDS_PER_SECOND * UPDATE_INTERVAL_IN_SECONDS;
+    // The fastest update frequency, in seconds
+    private static final int FASTEST_INTERVAL_IN_SECONDS = 1;
+    // A fast frequency ceiling in milliseconds
+    private static final long FASTEST_INTERVAL =
+            MILLISECONDS_PER_SECOND * FASTEST_INTERVAL_IN_SECONDS;
+
+    // Define an object that holds accuracy and frequency parameters
+    LocationRequest mLocationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mLocationRequest = LocationRequest.create();
+        // Use high accuracy
+        mLocationRequest.setPriority(
+                LocationRequest.PRIORITY_HIGH_ACCURACY);
+        // Set the update interval to 5 seconds
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        // Set the fastest update interval to 1 second
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+
         setContentView(R.layout.activity_tracker);
         Firebase.setAndroidContext(this);
         ref = new Firebase(FIREBASE_URL);
@@ -59,7 +88,6 @@ public class TrackerActivity extends ActionBarActivity implements
 
         // Connect the client.
         mLocationClient.connect();
-
     }
 
     /*
@@ -68,7 +96,7 @@ public class TrackerActivity extends ActionBarActivity implements
     @Override
     protected void onStop() {
         // Disconnecting the client invalidates it.
-        mLocationClient.disconnect();
+        //mLocationClient.disconnect();
         super.onStop();
     }
 
@@ -94,7 +122,16 @@ public class TrackerActivity extends ActionBarActivity implements
 
     public void startTrackOnClick(View view) {
         mCurrentLocation = mLocationClient.getLastLocation();
-        ref.push().setValue(new CoordinateObject(mCurrentLocation.getLatitude(),mCurrentLocation.getLongitude()));
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        // Report to the UI that the location was updated
+        ref.push().setValue(new CoordinateObject(location.getLatitude(),location.getLongitude()));
+        ((TextView)findViewById(R.id.txtLat)).setText(location.getLatitude() + "");
+        ((TextView)findViewById(R.id.txtLong)).setText(location.getLongitude() + "");
+
     }
 
     /*
@@ -106,6 +143,7 @@ public class TrackerActivity extends ActionBarActivity implements
     public void onConnected(Bundle dataBundle) {
         // Display the connection status
         Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+        mLocationClient.requestLocationUpdates(mLocationRequest, this);
     }
 
     /*
